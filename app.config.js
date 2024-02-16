ceb7331ba0b8109156226a9750cfcaa1ec412e17
selle0 <gisellemezazavala17@gmail.com>
2024-02-15 23:45:50 -0700

    todo el proyectito

--- app.config.js
+++ app.config.js
@@ -1,4 +1,5 @@
-{
+import 'dotenv/config';
+export default{
   "expo": {
     "name": "emoji-crud",
     "slug": "emoji-crud",
@@ -25,6 +26,14 @@
     },
     "web": {
       "favicon": "./assets/favicon.png"
+    },
+    extra: {
+      apiKey: "AIzaSyAUbMzI6LEDmPFSKJqPAJIAe3RGVurJUfA",
+      authDomain: "emoji-crud-27aae.firebaseapp.com",
+      projectId: "emoji-crud-27aae",
+      storageBucket: "emoji-crud-27aae.appspot.com",
+      messagingSenderId: "295423601791",
+      appId: "1:295423601791:web:883ac32f24eef3a143a34a"
     }
   }
 }
